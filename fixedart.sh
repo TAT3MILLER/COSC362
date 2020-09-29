8be3ddcf2595237082d3f102d5d0c8325d5e011d
TAT3MILLER <47550949+TAT3MILLER@users.noreply.github.com>
2020-09-28 21:50:40 -0400

    Add files via upload

---
+++ fixedart.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+
+# Script produces animation of rocketship blasting off
+
+f1()
+{
+    echo "        +       "
+    echo "       / \      "
+    echo "      /   \     "
+    echo "     /     \    "
+    echo "     |     |    "
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+}
+
+f2()
+{
+    echo "       / \      "
+    echo "      /   \     "
+    echo "     /     \    "
+    echo "     |     |    "
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "    **@@*@@**   "
+}
+
+
+f3()
+{
+    echo "      /   \     "
+    echo "     /     \    "
+    echo "     |     |    "
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "    @%%*%*@**   "
+    echo "   **%##@ *@**  "
+}
+
+
+f4()
+{
+    echo "     /     \    "
+    echo "     |     |    "
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "    **@@*@@**   "
+    echo "   **%##@ *@**  "
+    echo "    @%%*%*@**   "
+}
+
+f5()
+{
+    echo "     |     |    "
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "   *%@@@% ** @  "
+    echo "    @%%*%*@**   "
+    echo "   *%**@@%  @*  "
+    echo "   @*%**@***%@  "
+}
+
+f6()
+{
+    
+    echo "     |-----|    "
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "   @*%**@***%@  "
+    echo "   *%@@@% ** @  "
+    echo "   * %% @ @ **  "
+    echo "   @@   *%% **  "
+    echo "                "
+}
+
+f7()
+{
+
+    echo "     |_____|    "
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "    @**   % @@* "
+    echo "   *** %@* @*%  "
+    echo "   @%  *@ %%*   "
+    echo "   *%%  @** @   "
+    echo "                "
+    echo "                "
+}
+
+f8()
+{
+    echo "     |_____|    "
+    echo "    /| | | |\   "
+    echo "    ** @ %*@    "
+    echo "   @%@ ***@     "
+    echo "   @@ %* *@ *   "
+    echo "   **%%  @@**   "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f9()
+{
+
+    echo "    /| | | |\   "
+    echo "    %*@   %*@   "
+    echo "   @***@  **%   "
+    echo "   @ ** %% @ *  "
+    echo "   ** @%%*  %   "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f10()
+{
+    echo "     %*@ *@%@@  "
+    echo "   *@  *% **@%  "
+    echo "   @ *% *%@@    "
+    echo "   ** @%**%     "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f11()
+{
+    echo "   *@%   **%@   "
+    echo "   @*%@**  %    "
+    echo "   * @*%  **@   "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f12()
+{
+    echo "   %@**  @**    "
+    echo "   *@ @**%%*@   "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f13()
+{
+    echo "   *@ %*@** %   "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+
+f14()
+{
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+    echo "                "
+}
+f15()
+{
+
+    echo " BBBBBBBBBBBBBBBBB                                 TTTTTTTTTTTTTTTTTTTTTTT  iiii                               TTTTTTTTTTTTTTTTTTTTTTT                                                                "
+    echo " B::::::::::::::::B                                T:::::::::::::::::::::T i::::i                              T:::::::::::::::::::::T                                                                "
+    echo " B::::::BBBBBB:::::B                               T:::::::::::::::::::::T  iiii                               T:::::::::::::::::::::T                                                                "
+    echo " BB:::::B     B:::::B                              T:::::TT:::::::TT:::::T                                     T:::::TT:::::::TT:::::T                                                                "
+    echo "   B::::B     B:::::Byyyyyyy           yyyyyyy     TTTTTT  T:::::T  TTTTTTiiiiiii    mmmmmmm    mmmmmmm        TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrr      ooooooooooo   yyyyyyy           yyyyyyy  "
+    echo "   B::::B     B:::::B y:::::y         y:::::y              T:::::T        i:::::i  mm:::::::m  m:::::::mm              T:::::T        r::::rrr:::::::::r   oo:::::::::::oo  y:::::y         y:::::y   "
+    echo "   B::::BBBBBB:::::B   y:::::y       y:::::y               T:::::T         i::::i m::::::::::mm::::::::::m             T:::::T        r:::::::::::::::::r o:::::::::::::::o  y:::::y       y:::::y    "
+    echo "   B:::::::::::::BB     y:::::y     y:::::y                T:::::T         i::::i m::::::::::::::::::::::m             T:::::T        rr::::::rrrrr::::::ro:::::ooooo:::::o   y:::::y     y:::::y     "
+    echo "   B::::BBBBBB:::::B     y:::::y   y:::::y                 T:::::T         i::::i m:::::mmm::::::mmm:::::m             T:::::T         r:::::r     r:::::ro::::o     o::::o    y:::::y   y:::::y      "
+    echo "   B::::B     B:::::B     y:::::y y:::::y                  T:::::T         i::::i m::::m   m::::m   m::::m             T:::::T         r:::::r     rrrrrrro::::o     o::::o     y:::::y y:::::y       "
+    echo "   B::::B     B:::::B      y:::::y:::::y                   T:::::T         i::::i m::::m   m::::m   m::::m             T:::::T         r:::::r            o::::o     o::::o      y:::::y:::::y        "
+    echo "   B::::B     B:::::B       y:::::::::y                    T:::::T         i::::i m::::m   m::::m   m::::m             T:::::T         r:::::r            o::::o     o::::o       y:::::::::y         "
+    echo " BB:::::BBBBBB::::::B        y:::::::y                   TT:::::::TT      i::::::im::::m   m::::m   m::::m           TT:::::::TT       r:::::r            o:::::ooooo:::::o        y:::::::y          "
+    echo " B:::::::::::::::::B          y:::::y                    T:::::::::T      i::::::im::::m   m::::m   m::::m           T:::::::::T       r:::::r            o:::::::::::::::o         y:::::y           "
+    echo " B::::::::::::::::B          y:::::y                     T:::::::::T      i::::::im::::m   m::::m   m::::m           T:::::::::T       r:::::r             oo:::::::::::oo         y:::::y            "
+    echo " BBBBBBBBBBBBBBBBB          y:::::y                      TTTTTTTTTTT      iiiiiiiimmmmmm   mmmmmm   mmmmmm           TTTTTTTTTTT       rrrrrrr               ooooooooooo          y:::::y             "
+    echo "                           y:::::y                                                                                                                                               y:::::y              "
+    echo "                          y:::::y                                                                                                                                               y:::::y               "
+    echo "                         y:::::y                                                                                                                                               y:::::y                "
+    echo "                        y:::::y                                                                                                                                               y:::::y                 "
+    echo "                       yyyyyyy                                                                                                                                               yyyyyyy                  "
+}
+
+
+f1
+sleep .1
+clear
+f2
+sleep .1
+clear
+f3
+sleep .1
+clear
+f4
+sleep .1
+clear
+f5
+sleep .1
+clear
+f6
+sleep .1
+clear
+f7
+sleep .1
+clear
+f8
+sleep .1
+clear
+f9
+sleep .1
+clear
+f10
+sleep .1
+clear
+f11
+sleep .1
+clear
+f12
+sleep .1
+clear
+f13
+sleep .1
+clear
+f14
+sleep .1
+clear
+f15
+sleep 1
+clear
+
